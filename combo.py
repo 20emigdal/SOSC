a97165a9ec2225ffebfbbd606c045658a957ee38
Eliza Migdal <20emigdal@castilleja.org>
2022-05-31 14:30:49 -0500

    made frequency tables and geocoded

--- combo.py
+++ combo.py
@@ -1,7 +1,7 @@
 import csv
 import geocoder
 
-with open('cities2020_utf8.csv','r') as file1, open('citieslist.csv','w') as file2:
+with open('cities2019_utf8.csv','r') as file1, open('citieslist19.csv','w') as file2:
 	csvFile = csv.reader(file1)
 	unique_cities = []
 	city_counts = []
@@ -9,16 +9,27 @@ with open('cities2020_utf8.csv','r') as file1, open('citieslist.csv','w') as fil
 	lons = []
 	for row in csvFile:
 		for chunk in row:
-			if not chunk in unique_cities:
-				unique_cities.append(chunk)
-				city_counts.append(1)
-				g = geocoder.osm(chunk)
-				latlng = g.latlng
-				lats.append(latlng[0])
-				lons.append(latlng[1])			
+			if chunk == '':
+				break
 			else:
-				index = unique_cities.index(chunk)
-				city_counts[index] += 1
+				if '¬†' in chunk:
+					splitup = chunk.split('¬†')
+					chunk = splitup[0] + splitup[1]
+				if not chunk in unique_cities:
+					print(chunk)
+					unique_cities.append(chunk)
+					city_counts.append(1)
+					g = geocoder.osm(chunk)
+					latlng = g.latlng
+					if latlng != None:	
+						lats.append(latlng[0])
+						lons.append(latlng[1])
+					else:
+						lats.append(0)
+						lons.append(0)			
+				else:
+					index = unique_cities.index(chunk)
+					city_counts[index] += 1
 	csvwriter = csv.writer(file2)
 	cols = ['city','frequency','latitude','longitude']
 	csvwriter.writerow(cols)
